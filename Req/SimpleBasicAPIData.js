74efc8850e651c3556d6acb7c32090d1d69d9101
unknown <anubhavgoyal12062001@gmail.com>
2024-07-05 22:04:26 +0530

    Learned a new module

--- Req/SimpleBasicAPIData.js
+++ Req/SimpleBasicAPIData.js
@@ -1,2 +1,8 @@
-const data = {name:"Anubhav Goel", email:"anubhavgoyal12062001@gmail.com"}
+// const data = {name:"Anubhav Goel", email:"anubhavgoyal12062001@gmail.com"}
+const data = [{name:'Anubhav Goel 1', Mobile_No:'100'},
+    {name:'Anubhav Goel 2', Mobile_No:'1001'},
+    {name:'Anubhav Goel 3', Mobile_No:'1002'},
+    {name:'Anubhav Goel 4', Mobile_No:'1003'},
+    {name:'Anubhav Goel 5', Mobile_No:'1004'},]
+
 module.exports=data;
