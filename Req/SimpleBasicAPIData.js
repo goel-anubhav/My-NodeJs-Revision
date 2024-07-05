6ec9ac14c515ef9d41d11482f942ca0c2c8aa694
unknown <anubhavgoyal12062001@gmail.com>
2024-07-05 20:16:11 +0530

    Learned a new module

---
+++ Req/SimpleBasicAPIData.js
@@ -0,0 +1,2 @@
+const data = {name:"Anubhav Goel", email:"anubhavgoyal12062001@gmail.com"}
+module.exports=data;
--- simple-Basic-API.js
+++ simple-Basic-API.js
@@ -1,6 +1,17 @@
-const http = require("http");
- http.createServer((req,resp)=>{
-resp.writeHead(200,{'Content-Type':'application\json'});
-resp.write(JSON.stringify({name:'Anubhav',email:'anubhav@123'}) );
-resp.end();
-}).listen(1001);
+// const http = require("http");
+//  http.createServer((req,resp)=>{
+// resp.writeHead(200,{'Content-Type':'application\json'});
+// resp.write(JSON.stringify({name:'Anubhav',email:'anubhav@123'}) );
+// resp.end();
+// }).listen(1001);
+
+
+// If the data is another file like I'm creating a file in the folder Req/SimpleBasicAPIData.js
+
+const data = require('./Req/SimpleBasicAPIData')
+const http = require('http')
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify(data))
+    resp.end();
+}).listen(1001)
