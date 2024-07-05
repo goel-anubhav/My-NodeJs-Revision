e88995820a50667dee77e6a61bca0cd156e13eeb
unknown <anubhavgoyal12062001@gmail.com>
2024-07-05 22:04:54 +0530

    Learned a new module

--- Req/SimpleBasicAPIData.js
+++ Req/SimpleBasicAPIData.js
@@ -1,4 +1,4 @@
-// const data = {name:"Anubhav Goel", email:"anubhavgoyal12062001@gmail.com"}
+// const data = {name:"Anubhav Goel", email:"anubhavgoyal12062001@gmail.com"} // In case of only one data output
 const data = [{name:'Anubhav Goel 1', Mobile_No:'100'},
     {name:'Anubhav Goel 2', Mobile_No:'1001'},
     {name:'Anubhav Goel 3', Mobile_No:'1002'},
