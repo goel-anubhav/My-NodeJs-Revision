5108fc3880911baea9cc220054ea0c4a31eb5a73
unknown <anubhavgoyal12062001@gmail.com>
2024-07-08 23:45:04 +0530

    Learned a new module

--- CRUD In File System/UpdateFile.js
+++ CRUD In File System/UpdateFile.js
@@ -3,6 +3,6 @@ const path = require('path')
 const dirPath=path.join(__dirname, 'crud');
 const filePath = `${dirPath}/crud.txt`
 
-fs.appendFile(filePath,'and I am a software developer',(err)=>{ //Append Multiple Times
+fs.appendFile(filePath,' and I am a software developer',(err)=>{ //Append Multiple Times
     if(!err) console.log("File Updated Sir")
 })
