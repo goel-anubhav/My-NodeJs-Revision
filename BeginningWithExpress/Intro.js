ee7994da37415ee9c4a2486fac5c668289d22603
unknown <anubhavgoyal12062001@gmail.com>
2024-07-09 00:59:42 +0530

    Learned a new module

--- BeginningWithExpress/Intro.js
+++ BeginningWithExpress/Intro.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express();
 
 app.get('', (req, res)=>{
+    console.log("Data Sent By Browser",req.query) // http://localhost:5000/?name=anubhav
+    console.log("Data Sent By Browser",req.query.name) // http://localhost:5000/?name=anubhav
+
     res.send("Hello, this is my homepage")
 });
 
