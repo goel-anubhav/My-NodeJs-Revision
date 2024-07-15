5229d5cbb14e4ab5ce9ea3ecba716ac324fc1521
unknown <anubhavgoyal12062001@gmail.com>
2024-07-15 17:33:57 +0530

    Learning & Updating

--- MiddleWare/IntroMiddleWare.js
+++ MiddleWare/IntroMiddleWare.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express();
 
 const reqFilter=(req, resp, next)=>{
-    console.log('reqFilter');
-    next();
+    if(!req.query.age){
+        resp.send("Please Provide Age To Proceed")
+    }
+    else if(req.query.age<18){
+        resp.send("You are not allowed to access the page")
+    }
+    else{
+
+        next();
+    }
 }
 app.use(reqFilter)
 
