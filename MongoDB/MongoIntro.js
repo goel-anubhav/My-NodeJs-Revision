49549a0df76aa25d80cf86dfc34574bf058c2cba
unknown <anubhavgoyal12062001@gmail.com>
2024-07-16 19:57:44 +0530

    Learning & Updating

---
+++ MongoDB/MongoIntro.js
@@ -0,0 +1,16 @@
+const {MongoClient} = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient     //Another Method to use MongoDb
+const database='e-com'
+const url = 'mongodb://localhost:27017'
+const client= new MongoClient(url);
+
+async function getData()
+{
+    let result = await client.connect();
+    let db= result.db(database);
+    let collection = db.collection('products');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
+
+getData()
