5ff3336f599b4ae3a4d9fef1c8486fdb5e38e281
unknown <anubhavgoyal12062001@gmail.com>
2024-07-17 18:35:51 +0530

    POST API Learnt

--- Basic API/PostAPI.js
+++ Basic API/PostAPI.js
@@ -2,11 +2,22 @@ const express = require('express')
 const dbconnect = require('../MongoDB/ReadData2Config')
 const app = express();
 
-app.use(express.json)
-app.post('/',async(req,resp)=>{
-    console.log(req.body)
+app.use(express.json());
+//GET
+app.get('/',async(req,resp)=>{
     let data = await dbconnect();
-    let result = data.insertOne(req.body)
+    data=await data.find().toArray();
+    console.log(data)
+    resp.send(data)
+});
+
+//POST
+app.post('/',async(req,resp)=>{
+    let data = await dbconnect();
+    let result = await data.insertOne(req.body)
+
     resp.send(result)
 })
-app.listen(7000)
+app.listen(5000)
+
+
