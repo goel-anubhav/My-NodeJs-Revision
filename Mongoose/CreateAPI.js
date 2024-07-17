86fa7b5ca544c172cc1b43be67f2c22ff12a2b03
unknown <anubhavgoyal12062001@gmail.com>
2024-07-17 23:26:25 +0530

    Create With Mongoose

--- Mongoose/CreateAPI.js
+++ Mongoose/CreateAPI.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const main = async()=>{
     await mongoose.connect("mongodb://localhost:27017/e-com")
-    // Schemas are used to validate the fields
 
     const ProductSchema = new mongoose.Schema({
         name:String,
@@ -10,8 +9,6 @@ const main = async()=>{
         brand:String,
         Category:String
     })
-
-    //Models are used to use schemas to connect node with mongodb
 
     const ProductModel = mongoose.model('products',ProductSchema);
     let data = new ProductModel({name:"m11",price:1000,brand:'xyz',Category:"Mobile"});
--- gitpush.ps1
+++ gitpush.ps1
@@ -1,3 +1,3 @@
 git add .
-git commit -m "LCreate With Mongoose"
+git commit -m "Create With Mongoose"
 git push  
