dba82f50d1fa82c40f0c3320845243f62256554e
unknown <anubhavgoyal12062001@gmail.com>
2024-07-17 22:46:37 +0530

    POST API

--- Basic API/PutAPI.js
+++ Basic API/PutAPI.js
@@ -4,14 +4,26 @@ const app = express();
 
 app.use(express.json());
 //PUT Hardcoded data
-app.put('/',async(req,resp)=>{
-     let data = await dbconnect();
-     let result = data.updateOne(
-        {name:"Omega"},
-        { $set:{name:"Omega 2"}}
-    )
-    resp.send({result:'update'})
+// app.put('/',async(req,resp)=>{
+//      let data = await dbconnect();
+//      let result = data.updateOne(
+//         {name:"Omega"},
+//         { $set:{name:"Omega 2"}}
+//     )
+//     resp.send({result:'update'})
+// })
+
+
+app.put('/:name',async(req,resp)=>{
+    let data = await dbconnect();
+    let result = await data.updateOne(
+       {name:req.params.name},    // params is used to map
+       { $set:req.body}
+   )
+   resp.send({result:'update'})
 })
+
+
 app.listen(5000)
 
 
